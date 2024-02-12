8bd68d4f7b575c053f203fdc429d6f5d6f79b2de
CukiD <40711562+Tektek9@users.noreply.github.com>
2024-02-12 13:12:39 +0700

    mr.malik

--- malik.py
+++ malik.py
@@ -13,34 +13,35 @@ class malik():
     def main(self):
         ip = {"q":self.xx}
         pwn = requests.request("GET", self.aa, params=ip)
-        return print(pwn.text)
+        return pwn.text
 
 if __name__ == "__main__":
     api = "http://api.hackertarget.com/reverseiplookup/"
     if len(sys.argv) < 2:
         malik.logo()
-        print("Untuk bantuan:\n  python reverseIP.py -h/--help\n")
+        print("Untuk bantuan:\n  python malik.py -h/--help\n")
     elif str(sys.argv[1]) == "-h" or str(sys.argv[1]) == "--help":
         malik.logo()
-        print("python reverseIP.py [target]\npython reverseIP.py [target] -o/--output [output.txt]\n")
-    elif str(sys.argv[1]) != "-h" or str(sys.argv[1]) != "--help":
+        print("python malik.py [target]\npython malik.py [target] -o/--output [output.txt]\n")
+    elif len(sys.argv) == 2:
         x = str(sys.argv[1])
         malik.logo()
         mulai = malik(x, api)
-        mulai.main()
-        print("")
-    elif len(sys.argv) < 3:
-        if str(sys.argv[2]) == "-o" or str(sys.argv[2]) == "--output":
+        uu = mulai.main()
+        print(f"{uu}\n")
+    elif len(sys.argv) == 4:
+        if (str(sys.argv[2]) == "-o" or str(sys.argv[2]) == "--output") and '.txt' in str(sys.argv[3]).lower():
             x = str(sys.argv[1])
+            f = sys.argv[3]
             mulai = malik(x, api)
             malik.logo()
             haha = mulai.main()
-            file = open(str(sys.argv[3]),'w')
-            for anjay in haha:
-                file.write(str(anjay)+"\n")
+            file = open(str(f),'w')
+            for an in haha:
+                file.write(str(an)+"\n")
             file.close()
             print(f"Output berhasil disimpan dengan mana {str(sys.argv[3])}\n")
         else:
             pass
     else:
-        pass
+        print("\nUntuk bantuan:\n  python malik.py -h/--help\n")
